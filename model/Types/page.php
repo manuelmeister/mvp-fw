516f5bf3334c18ad2152b6e99d369727f5a440b1
bmeism <news.manuelsworld@gmail.com>
2015-02-12 08:06:11 +0100

    Added page type

---
+++ model/Types/page.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bmeism
+ * Date: 11.02.2015
+ * Time: 13:59
+ */
+
+namespace Meister\Model\Types;
+
+class Page extends Types {
+
+    function getQuery()
+    {
+        if(is_numeric($this->param)){
+            return "SELECT * FROM PAGE WHERE ID='".(int) $this->param."'";
+        }else {
+            return "SELECT * FROM PAGE WHERE slug='$this->param'";
+        }
+    }
+
+}
