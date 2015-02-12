c8d3338fbabb5c1d48f0a4ed137c125e2db9c65d
bmeism <news.manuelsworld@gmail.com>
2015-02-12 15:58:53 +0100

    Corrected syntax

--- loader.php
+++ loader.php
@@ -3,6 +3,6 @@
 namespace Meister;
 
 spl_autoload_register(function ($class) {
-    $class_path = str_replace('Meister','',str_replace('\\', '/', $class));
+    $class_path = str_replace('Meister/','',str_replace('\\', '/', $class));
     include __DIR__.'/'.$class_path.'.php';
 });
