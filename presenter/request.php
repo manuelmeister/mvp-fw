37e2f96f88a35f2a10f5dadcb32e6b0f4f88522e
bmeism <news.manuelsworld@gmail.com>
2015-02-12 15:57:34 +0100

    Added comments

--- presenter/request.php
+++ presenter/request.php
@@ -2,32 +2,61 @@
 
 namespace Meister\Presenter;
 
+/**
+ * Class Request
+ * @package Meister\Presenter
+ */
 class Request {
 
+    /**
+     * @var array
+     */
     private $url;
+    /**
+     * @var string
+     */
     private $page;
+    /**
+     * @var string
+     */
     private $action;
+    /**
+     * @var string
+     */
     private $param;
 
+    /**
+     * @param $url
+     */
     function __construct($url){
         $this->url = $url;
         $this->process();
     }
 
+    /**
+     * this will trim and explode the url
+     */
     function process(){
-        $this->url = rtrim($this->url, "/");
-
-        $this->url = explode("/",$this->url);
+        $this->url = explode("/",rtrim($this->url, "/"));
     }
 
+    /**
+     * @return string get page type
+     */
     public function getPage(){
         return isset($this->url[0]) ? $this->url[0] : 'index';
     }
 
+    /**
+     * @return string get slug or id of the content
+     */
     public function getParam(){
         return isset($this->url[1]) ? $this->url[1] : '1';
     }
 
+    /**
+     * @return string get action
+     */
     public function getAction(){
         return isset($this->url[2]) ? $this->url[2] : 'show';
     }
